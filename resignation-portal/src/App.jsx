f31556d058d86594037d8b2d68622859b5c9d418
maddie0501 <madhurikavatekar01@gmail.com>
2025-07-29 00:41:08 +0530

    frontend changes

--- resignation-portal/src/App.jsx
+++ resignation-portal/src/App.jsx
@@ -1,31 +1,27 @@
-import { Routes, Route, Navigate } from 'react-router-dom';
-import Login from './pages/Login';
-import Register from './pages/Register';
-import EmployeeDashboard from './pages/EmployeeDashboard';
-import AdminDashboard from './pages/AdminDashboard';
-import './app.css'
-
+import { Routes, Route, Navigate } from "react-router-dom";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import EmployeeDashboard from "./pages/EmployeeDashboard";
+import AdminDashboard from "./pages/AdminDashboard";
 
 function App() {
-  const token = localStorage.getItem('token');
-  const role = localStorage.getItem('role');
+  const token = localStorage.getItem("token");
+  const role = localStorage.getItem("role");
 
   return (
     <>
-
-     <Routes>
-      <Route path="/login" element={<Login />} />
-      <Route path="/" element={<Register />} />
-      {token && role === 'employee' && (
-        <Route path="/employee" element={<EmployeeDashboard />} />
-      )}
-      {token && role === 'admin' && (
-        <Route path="/admin" element={<AdminDashboard />} />
-      )}
-      <Route path="*" element={<Navigate to="/" />} />
-    </Routes>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/" element={<Register />} />
+        {token && role === "employee" && (
+          <Route path="/employee" element={<EmployeeDashboard />} />
+        )}
+        {token && role === "admin" && (
+          <Route path="/admin" element={<AdminDashboard />} />
+        )}
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
     </>
-   
   );
 }
 
